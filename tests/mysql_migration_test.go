dc56f311414bb6e9f70a350b271ec24cafdac68b
Tauki Tahmid <ttauki@gmail.com>
2019-03-27 04:42:21 +0600

    add initial tests for mysql migration

--- tests/mysql_migration_test.go
+++ tests/mysql_migration_test.go
@@ -5,8 +5,41 @@ import (
 	"testing"
 )
 
+// todo: configure this test to be run on a dev env
+
+func TestConfirmDatabase(t *testing.T) {
+
+	migration, _ := scripts.GetMigrationService(Config)
+
+	err := migration.ConfirmDatabase()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDropTables(t *testing.T) {
+
+	migration, _ := scripts.GetMigrationService(Config)
+
+	err := migration.DropTables()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDropDb(t *testing.T) {
+
+	migration, _ := scripts.GetMigrationService(Config)
+
+	err := migration.DropDb()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
 func TestInitMigrate(t *testing.T) {
-	migration := scripts.GetMigrationService(Config)
+
+	migration, _ := scripts.GetMigrationService(Config)
 
 	err := migration.InitMigrate()
 	if err != nil {
